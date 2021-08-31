42cb9ca3c95cce2cf1a6617228c2708f16d1e686
FairaIchsa <zelov02@yandex.ru>
2021-08-31 18:44:18 +0300

    made To Do app

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class ToDo(models.Model):
+
+    name = models.CharField(max_length=100)
+    due_date = models.DateField()
+
+    def __str__(self):
+        return "{} ({})".format(self.name, self.due_date)
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .forms import ToDoForm
+from .models import ToDo
+
+
+def todo_list(request):
+    todos = ToDo.objects.all()
+    context = {
+        "todo_list": todos
+    }
+    return render(request, "todo_list.html", context)
+
+
+def todo_detail(request, id):
+    todo = ToDo.objects.get(id=id)
+    context = {
+        "todo": todo
+    }
+    return render(request, "todo_detail.html", context)
+
+
+def todo_create(request):
+    form = ToDoForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('/')
+    context = {"form": form}
+    return render(request, "todo_create.html", context)
+
+
+def todo_update(request, id):
+    todo = ToDo.objects.get(id=id)
+    form = ToDoForm(request.POST or None, instance=todo)
+    if form.is_valid():
+        form.save()
+        return redirect('/')
+    context = {"form": form}
+    return render(request, "todo_update.html", context)
+
+
+def todo_delete(request, id):
+    todo = ToDo.objects.get(id=id)
+    todo.delete()
+    return redirect('/')
