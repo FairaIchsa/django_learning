75c0eab84aae83fb83d62f3d7c4201393f93e294
FairaIchsa <zelov02@yandex.ru>
2021-09-07 15:19:31 +0300

    made create view

--- djcrm/leads/urls.py
+++ djcrm/leads/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import lead_list, lead_detail
+from .views import lead_list, lead_detail, lead_create
 
 app_name = "leads"
 
 urlpatterns = [
     path('', lead_list),
-    path('<pk>/', lead_detail),
+    path('<int:pk>/', lead_detail),  # int нужно для того, чтобы create не воспринималось, как pk
+    path('create/', lead_create),
 ]
